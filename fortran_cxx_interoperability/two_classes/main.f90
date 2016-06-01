0fbca94e38830bec876ef598d62a5a984da4f295
Tatsuya Sakashita <tatsuya.sakashita@apchem.nagoya-u.ac.jp>
2016-06-02 08:32:40 +0900

    add functor module in Fortran

--- fortran_cxx_interoperability/two_classes/main.f90
+++ fortran_cxx_interoperability/two_classes/main.f90
@@ -25,15 +25,56 @@ contains
   end subroutine integrate_trapezoid
 end module integration_mod
 
+module fortran_functor_mod
+  use base_functor_mod
+  implicit none
+
+  type, extends(base_functor_type) :: fortran_functor_type
+     real(8) :: a, b
+   contains
+     procedure, pass(this) :: eval => f
+  end type fortran_functor_type
+
+  interface construct
+    module procedure fortran_functor_construct
+ end interface construct
+ 
+  interface destruct
+    module procedure fortran_functor_destruct
+ end interface destruct
+ 
+contains
+  real(8) function f(this, x)
+    class(fortran_functor_type), intent(in) :: this
+    real(8), value, intent(in) :: x
+    print *, "This is f"
+    f = exp(-this%a*x) * cos(this%b*x)
+  end function f
+
+  subroutine fortran_functor_construct(this, a_in, b_in)
+    type(fortran_functor_type), intent(inout) :: this
+    real(8), intent(in) :: a_in, b_in
+    this%a = a_in
+    this%b = b_in
+  end subroutine fortran_functor_construct
+  
+  subroutine fortran_functor_destruct(this)
+    type(fortran_functor_type), intent(inout) :: this
+  end subroutine fortran_functor_destruct
+  
+end module fortran_functor_mod
+
 
 program main
   use integration_mod
   use user_functor1_mod
   use user_functor2_mod
+  use fortran_functor_mod
   implicit none
 
   type(user_functor1_type) :: this, that
   type(user_functor2_type) :: this2
+  type(fortran_functor_type) :: this3
 
   real(8) :: xmin, xmax, s
   integer :: n
@@ -56,5 +97,10 @@ program main
   call integrate_trapezoid(this2, xmin, xmax, n, s)
   write(*,*) 'Result: ', s
   call destruct(this2)
+
+  call construct(this3, 1.d0, 2.d0)
+  call integrate_trapezoid(this3, xmin, xmax, n, s)
+  write(*,*) 'Result: ', s
+  call destruct(this3)
   
 end program main
