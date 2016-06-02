ad9a588d334af200698b1cb182b176e5a39a345f
Tatsuya Sakashita <tatsuya.sakashita@apchem.nagoya-u.ac.jp>
2016-06-02 10:08:48 +0900

    fix indent

--- fortran_cxx_interoperability/two_classes/user_functor1_wrapper.f90
+++ fortran_cxx_interoperability/two_classes/user_functor1_wrapper.f90
@@ -10,33 +10,33 @@ module user_functor1_mod
 
   interface
      function user_functor1_construct_c (a, b) result(ptr) bind(c,name="user_functor1_construct")
-      import
-      real(8), value, intent(in) :: a, b
-      type(c_ptr) :: ptr
-    end function user_functor1_construct_c
-    subroutine user_functor1_destruct_c (ptr) bind(c,name="user_functor1_destruct")
-      import
-      type(c_ptr), value, intent(in) :: ptr
-    end subroutine user_functor1_destruct_c    
-    function user_functor1_eval_c (ptr, x) result(y) bind(c,name="user_functor1_eval")
-      use iso_c_binding
-      import
-      type(c_ptr), value, intent(in) :: ptr
-      real(8), value, intent(in) :: x
-      real(8) :: y
-    end function user_functor1_eval_c
- end interface
-
- interface construct
-    module procedure user_functor1_construct
- end interface construct
- 
- interface destruct
-    module procedure user_functor1_destruct
- end interface destruct
- 
+       import
+       real(8), value, intent(in) :: a, b
+       type(c_ptr) :: ptr
+     end function user_functor1_construct_c
+     subroutine user_functor1_destruct_c (ptr) bind(c,name="user_functor1_destruct")
+       import
+       type(c_ptr), value, intent(in) :: ptr
+     end subroutine user_functor1_destruct_c
+     function user_functor1_eval_c (ptr, x) result(y) bind(c,name="user_functor1_eval")
+       use iso_c_binding
+       import
+       type(c_ptr), value, intent(in) :: ptr
+       real(8), value, intent(in) :: x
+       real(8) :: y
+     end function user_functor1_eval_c
+  end interface
+  
+  interface construct
+     module procedure user_functor1_construct
+  end interface construct
+  
+  interface destruct
+     module procedure user_functor1_destruct
+  end interface destruct
+  
 contains
-
+  
   subroutine user_functor1_construct(this,a,b)
     type(user_functor1_type), intent(out) :: this
     real(8), intent(in) :: a, b
--- fortran_cxx_interoperability/two_classes/user_functor2_wrapper.f90
+++ fortran_cxx_interoperability/two_classes/user_functor2_wrapper.f90
@@ -7,34 +7,34 @@ module user_functor2_mod
    contains
      procedure, pass(this) :: eval => user_functor2_eval
   end type user_functor2_type
-
+  
   interface
      function user_functor2_construct_c (m) result(ptr) bind(c,name="user_functor2_construct")
-      import
-      integer, value, intent(in) :: m
-      type(c_ptr) :: ptr
-    end function user_functor2_construct_c
-    subroutine user_functor2_destruct_c (ptr) bind(c,name="user_functor2_destruct")
-      import
-      type(c_ptr), value, intent(in) :: ptr
-    end subroutine user_functor2_destruct_c    
-    function user_functor2_eval_c (ptr, x) result(y) bind(c,name="user_functor2_eval")
-      use iso_c_binding
-      import
-      type(c_ptr), value, intent(in) :: ptr
-      real(8), value, intent(in) :: x
-      real(8) :: y
-    end function user_functor2_eval_c
- end interface
-
- interface construct
-    module procedure user_functor2_construct
- end interface construct
- 
- interface destruct
-    module procedure user_functor2_destruct
- end interface destruct
- 
+       import
+       integer, value, intent(in) :: m
+       type(c_ptr) :: ptr
+     end function user_functor2_construct_c
+     subroutine user_functor2_destruct_c (ptr) bind(c,name="user_functor2_destruct")
+       import
+       type(c_ptr), value, intent(in) :: ptr
+     end subroutine user_functor2_destruct_c
+     function user_functor2_eval_c (ptr, x) result(y) bind(c,name="user_functor2_eval")
+       use iso_c_binding
+       import
+       type(c_ptr), value, intent(in) :: ptr
+       real(8), value, intent(in) :: x
+       real(8) :: y
+     end function user_functor2_eval_c
+  end interface
+  
+  interface construct
+     module procedure user_functor2_construct
+  end interface construct
+  
+  interface destruct
+     module procedure user_functor2_destruct
+  end interface destruct
+  
 contains
   
   subroutine user_functor2_construct(this,m)
